aeeb1309f6dedd3e7432352df6bdce2eafbe2141
anassu1400 <anassu1400@gmail.com>
2019-02-14 14:01:30 +0300

    Done with mini p

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
-
+from .forms import CarForm
+from django.contrib import messages
 def car_list(request):
 	cars = Car.objects.all()
 	context = {
@@ -18,15 +19,34 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES or None)
+		if form.is_valid():
+				form.save()
+				return redirect('car-list')
+	context={
+		'create_form': form,
+	}
+	return render(request, 'create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+	car = Car.objects.get(id=car_id)
+	form = CarForm(instance=car)
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES or None, instance=car)
+		if form.is_valid():
+				form.save()
+				messages.success(request, 'car details updated.')
+				return redirect('car-list')
+	context={
+		'car': car,
+		'update_form': form,
+	}
+	return render(request, 'update.html', context)
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	Car.objects.get(id=car_id).delete()
+	return redirect('car-list')
